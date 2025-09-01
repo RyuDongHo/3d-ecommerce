5ec5c2955fd044d120375335cd2d2dd48996dd08
unknown <thffhahs03@gmail.com>
2025-09-02 01:20:13 +0900

    chore: index.html & package index.html title 수정 및 package 추가

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,9 +1,23 @@
+import { Canvas } from "@react-three/fiber";
 import ShowRoom from "./ui";
+import { OrbitControls } from "@react-three/drei";
 
 const Home = () => {
   return (
     <div className="w-full h-full">
-      <ShowRoom />
+      <Canvas
+        shadows
+        camera={{ fov: 90, position: [5, 5, 5], near: 0.1, far: 1000 }}
+      >
+        <color attach="background" args={["white"]} />
+        <OrbitControls
+          // minAzimuthAngle={-Math.PI / 4}
+          // maxAzimuthAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 4}
+          maxPolarAngle={Math.PI / 1}
+        />
+        <ShowRoom />
+      </Canvas>
     </div>
   );
 };
--- src/Page/Home/ui/index.tsx
+++ src/Page/Home/ui/index.tsx
@@ -1,34 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-
 const ShowRoom = () => {
   return (
-    <div className="w-full h-full">
-      <Canvas
-        shadows
-        camera={{ fov: 90, position: [5, 5, 5], near: 0.1, far: 1000 }}
-      >
-        <color attach="background" args={["white"]} />
-        <OrbitControls
-          // minAzimuthAngle={-Math.PI / 4}
-          // maxAzimuthAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 1}
+    <>
+      <mesh>
+        <directionalLight
+          position={[5, 5, 5]}
+          target-position={[0, 0, 0]}
+          intensity={7}
+          castShadow
         />
-        <mesh>
-          <directionalLight
-            position={[5, 5, 5]}
-            target-position={[0, 0, 0]}
-            intensity={7}
-            castShadow
-          />
-          <ambientLight intensity={0.5} />
-          <gridHelper args={[10, 10]} />
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      </Canvas>
-    </div>
+        <ambientLight intensity={0.5} />
+        <gridHelper args={[10, 10]} />
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </>
   );
 };
 
